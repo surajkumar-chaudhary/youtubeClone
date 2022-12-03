ee0a769c290b6758578fdbb6d4e000b7264d3037
Suraj Chaudhary <suraj.181544@ncit.edu.np>
2022-12-03 10:15:59 +0545

    each video card compo. added

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import LeftNav from "./LeftNav";
+import { Context } from "../context/ContextApi";
+import VideoCard from "./VideoCard";
 
 const Feed = () => {
+  const { loading, searchResults } = useContext(Context);
+  console.log(searchResults);
+  useEffect(() => {
+    document.getElementById("root").classList.remove("custom-h");
+  }, []);
+
   return (
     <div className="flex flex-row h-[calc(100%-56px)]">
       <LeftNav />
-      left nav
+      <div className="grow w-[calc(100%-240px)] h-full overflow-y-auto bg-black">
+        <div className="grid text-white grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-5">
+          {!loading && Array.isArray(searchResults)
+            ? searchResults.map((item, index) => {
+                return <VideoCard key={index} video={item?.video} />;
+              })
+            : null}
+
+          <div className="text-white">hellot there</div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/LeftNav.jsx
+++ src/components/LeftNav.jsx
@@ -39,6 +39,18 @@ const LeftNav = () => {
   const { selectCategories, setSelectCategories, mobileMenu } =
     useContext(Context);
   const navigate = useNavigate();
+  const clickHandler = (name, type) => {
+    switch (type) {
+      case "category":
+        return setSelectCategories(name);
+      case "home":
+        return setSelectCategories(name);
+      case "menu":
+        return false;
+      default:
+        break;
+    }
+  };
   return (
     <div
       className={`md:block w-[240px] overflow-y-auto h-full py-4 bg-black absolute md:relative z-10 translate-x-[-240px] md:translate-x-0 transition-all ${
@@ -52,7 +64,10 @@ const LeftNav = () => {
               <LeftNavMenuItem
                 text={item.type === "home" ? "Home" : item.name}
                 icon={item.icon}
-                action={() => {}}
+                action={() => {
+                  clickHandler(item.name, item.type);
+                  navigate("/");
+                }}
                 className={`${
                   selectCategories === item.name ? "bg-white/[0.15]" : ""
                 }`}
